07bc0b095cdbdd990afe2d397391e43d4a96a5db
Steve Corwin <37982371+PrinceCorwin@users.noreply.github.com>
2022-02-21 20:54:40 -0600

    EditListModal seperate component

---
+++ src/components/iList/EditListModal.js
@@ -0,0 +1,93 @@
+import Backdrop from './Backdrop';
+import {
+  Button,
+  Input,
+  FormControl,
+  FormHelperText,
+  Heading,
+  Flex,
+  Alert,
+  AlertIcon,
+} from '@chakra-ui/react';
+export const EditListModal = ({
+  themeObj,
+  handleSubmit,
+  editList,
+  newName2,
+  setNewName2,
+  setAlertText2,
+  setEditList,
+  alertText2,
+}) => {
+  return (
+    <>
+      <Backdrop />
+      <Flex
+        zIndex="2000"
+        grow="1"
+        justify="center"
+        direction="column"
+        p={6}
+        position="absolute"
+        bg={themeObj.bgItem}
+        borderRadius="md"
+      >
+        <Heading size="md" py={3}>
+          Rename Your List
+        </Heading>
+        <form
+          label="Rename List"
+          onSubmit={handleSubmit}
+          style={{ width: '100%' }}
+        >
+          <FormControl>
+            <Input
+              variant="outline"
+              autoFocus
+              autoComplete="off"
+              type="text"
+              id="newName"
+              placeholder={`Rename ${editList}`}
+              isRequired
+              value={newName2}
+              onChange={e => setNewName2(e.target.value)}
+              bg={themeObj.bgItem}
+            />
+            <FormHelperText>
+              New name can not match another existing list
+            </FormHelperText>
+            <Flex mt={4} w="70%" justify="space-between">
+              <Button
+                variant="solid"
+                type="submit"
+                aria-label="Rename List"
+                colorScheme="green"
+              >
+                Rename
+              </Button>
+              <Button
+                variant="solid"
+                type="button"
+                onClick={() => {
+                  setAlertText2(null);
+                  setNewName2('');
+                  setEditList(false);
+                }}
+                aria-label="cancel"
+                colorScheme="red"
+              >
+                Cancel
+              </Button>
+            </Flex>
+          </FormControl>
+          {alertText2 && (
+            <Alert status="error" variant="subtle" mt={6} mb={6}>
+              <AlertIcon />
+              {alertText2}
+            </Alert>
+          )}
+        </form>
+      </Flex>
+    </>
+  );
+};
