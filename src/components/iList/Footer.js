ff65ac590a922f0b913352c12bf15f2dd4fd2856
Steve Corwin <37982371+PrinceCorwin@users.noreply.github.com>
2022-03-01 19:36:03 -0600

    added iLIST logo to footer

--- src/components/iList/Footer.js
+++ src/components/iList/Footer.js
@@ -1,22 +1,32 @@
 import { Flex, Text } from '@chakra-ui/react';
-
+import checkbox3 from '../../images/checkbox3.png';
 const Footer = ({ user, bg, color, length }) => {
   return (
-    <Flex
-      bg={bg}
-      color={color}
-      width="100%"
-      justifyContent="space-between"
-      py={1}
-      px={3}
+    // <Flex
+    //   bg={bg}
+    //   color={color}
+    //   width="100%"
+    //   justifyContent="space-between"
+    //   py={1}
+    //   px={3}
+    // >
+    <div
+      className="flexrow-between-center w-100 py-1 px-3"
+      style={{ color: color, backgroundColor: bg }}
     >
+      <div>
+        <img className="d-i w-20px" src={checkbox3} alt="iList logo" />
+        <p className="d-i"> iLIST</p>
+      </div>
       <p>
-        {length} List {length === 1 ? 'item' : 'items'}
+        {length} list {length === 1 ? 'item' : 'items'}
       </p>
+
       <Text align="end" w="250px" isTruncated>
         {user.email ? user.email : 'Demo Mode'}
       </Text>
-    </Flex>
+    </div>
+    // </Flex>
   );
 };
 
